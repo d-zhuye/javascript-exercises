4bb082e6529e6eec998f8f1e92fff1dbe2ae07d6
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2024-10-30 00:02:48 -0700

    Create function that removes specified arguments from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // take an array and some other argument
+    // then remove other arguments from that array
+    // I need some way to compare each array item with each removed item
+
+    let cleansedArray = array;
+
+    for (const items of args) {
+        cleansedArray = cleansedArray.filter(function (input) {
+            return input !== items;
+        })
+    }
+    return cleansedArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3, 4);
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
