36c3ef9d15889c37826d5e47f4af362175269dbe
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2024-10-30 00:19:24 -0700

    Create function to calculate the sum between two positive integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumOfAll = 0;
+    let minValue = num1;
+    let maxValue = num2;
+
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } 
+    
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+
+    if (num1 > num2) {
+        minValue = num2;
+        maxValue = num1;
+    }
+
+    for (let i = minValue; i <= maxValue; i++) {
+        sumOfAll += i;
+    }
+    return sumOfAll;
 
 };
 
