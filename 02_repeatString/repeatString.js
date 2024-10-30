68e3eadfc9dc8f69aae79ea54046fd40799f6da3
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2024-10-29 18:18:23 -0700

    Create conditional check if number is less than 0

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,15 @@
 const repeatString = function(string, num) {
-    let repeated = "";
-    for (let i = 0; i < num; i++) {
-        repeated += string;
+    let result = "";
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            result += string; 
+        }
         
+    } else {
+        result = "ERROR";
     }
-    return repeated;
+    return result;
+    
 };
 
 // Do not edit below this line
