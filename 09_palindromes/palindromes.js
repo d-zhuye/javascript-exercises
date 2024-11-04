ad9467fd27e61af3400595e662af1ecffbbd37ea
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2024-11-03 21:06:10 -0800

    Create function to verify if string is palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let wordCapitalized = word.toUpperCase();
+    let wordSplit = wordCapitalized.split("");
 
+    let reverseArray = [];
+
+    for (let i = wordSplit.length - 1; i >= 0; i--){
+        if (/^[a-zA-Z0-9]$/.test(wordSplit[i])) {
+            reverseArray.push(wordSplit[i]);
+        }
+    }
+
+    let joinedReverseArray = reverseArray.join("");
+
+    let filterWord = wordSplit.filter((char) => /^[a-zA-Z0-9]$/.test(char));
+    let joinedFilteredWord = filterWord.join("");
+
+    if (joinedFilteredWord == joinedReverseArray) {
+        return true;
+    } else {
+        return false;
+    };
 };
-
 // Do not edit below this line
 module.exports = palindromes;
